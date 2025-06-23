3d1f373865ac4b9b8f82b434353efcc021eb37c9
Yael RDF <yaeladmin@dev-services.lan>
2025-06-22 22:31:33 -0600

    nononno

--- PROYECTO-Cocina/Creacion.sql
+++ PROYECTO-Cocina/Creacion.sql
@@ -1,52 +1,53 @@
 CREATE TABLE Menu(
  No_guisado NUMBER(3) NOT NULL,
-Guisado NVARCHAR2(50),
-Precio NUMBER(6,2),
-PRIMARY KEY (No_guisado)
+    Guisado NVARCHAR2(50),
+    Precio NUMBER(6,2),
+    PRIMARY KEY (No_guisado)
 );
 
 CREATE TABLE Personal(
-RFC_Nom VARCHAR2(4) NOT NULL,
-RFC_Fecha NUMBER(6) NOT NULL,
-Nombre NVARCHAR2(50),
-ApellidoP NVARCHAR2(50),
-ApellidoM NVARCHAR2(50),
-Cargo NVARCHAR2(50),
--- Constraint REGEX
-Horario VARCHAR2(11) CHECK (REGEXP_LIKE(Horario, '^([0-1]?[0-9]|2[0-3]):[0-5][0-9]-([0-1]?[0-9]|2[0-3]):[0-5][0-9]$')),
-TelefonoCelular NUMBER(10),
-PRIMARY KEY (RFC_Nom,RFC_Fecha)
+    RFC_Nom VARCHAR2(4) NOT NULL,
+    RFC_Fecha NUMBER(6) NOT NULL,
+    Nombre NVARCHAR2(50),
+    ApellidoP NVARCHAR2(50),
+    ApellidoM NVARCHAR2(50),
+    Cargo NVARCHAR2(50),
+    -- Constraint REGEX
+    Horario VARCHAR2(11) CHECK (REGEXP_LIKE(Horario, '^([0-1]?[0-9]|2[0-3]):[0-5][0-9]-([0-1]?[0-9]|2[0-3]):[0-5][0-9]$')),
+    TelefonoCelular NUMBER(10),
+    PRIMARY KEY (RFC_Nom,RFC_Fecha)
 );
 
 CREATE TABLE Cliente(
-Num_telefonico NUMBER(10) NOT NULL,
-Calle NVARCHAR2(50),
-Numero NUMBER(10),
-Colonia NVARCHAR2(50),
-Alcaldia NVARCHAR2(50),
-CP NUMBER(5),
-Estado NVARCHAR2(50),
-PRIMARY KEY (Num_telefonico)
+    Num_telefonico NUMBER(10) NOT NULL,
+    Nombre NVARCHAR2(50),
+    Calle NVARCHAR2(50),
+    Numero NUMBER(10),
+    Colonia NVARCHAR2(50),
+    Alcaldia NVARCHAR2(50),
+    CP NUMBER(5),
+    Estado NVARCHAR2(50),
+    PRIMARY KEY (Num_telefonico)
 );  
 
 CREATE TABLE Pedido(
-Num_Pedido NUMBER(10),
-Fecha DATE,
-RFC_Nom VARCHAR2(4),
-RFC_Fecha NUMBER(6),
-Num_telefonico NUMBER(10),  
-Porciones NUMBER(2),
-No_guisado NUMBER(3),
-Notas NVARCHAR2(500),
-Subtotal NUMBER(6,2),  
-Extra NUMBER(6,2),     
-Total NUMBER(6,2),     
-Pagado NUMBER(1) CHECK (Pagado IN (0,1)),
-Estado_preparacion VARCHAR2(30),
-PRIMARY KEY (Num_Pedido),
-FOREIGN KEY (RFC_Nom,RFC_Fecha) REFERENCES Personal(RFC_Nom,RFC_Fecha),
-FOREIGN KEY (Num_telefonico) REFERENCES Cliente(Num_telefonico),
-FOREIGN KEY (No_guisado) REFERENCES Menu(No_guisado)
+    Num_Pedido NUMBER(10),
+    Fecha DATE,
+    RFC_Nom VARCHAR2(4),
+    RFC_Fecha NUMBER(6),
+    Num_telefonico NUMBER(10),  
+    Porciones NUMBER(2),
+    No_guisado NUMBER(3),
+    Notas NVARCHAR2(500),
+    Subtotal NUMBER(6,2),  
+    Extra NUMBER(6,2),     
+    Total NUMBER(6,2),     
+    Pagado NUMBER(1) CHECK (Pagado IN (0,1)),
+    Estado_preparacion VARCHAR2(30),
+    PRIMARY KEY (Num_Pedido),
+    FOREIGN KEY (RFC_Nom,RFC_Fecha) REFERENCES Personal(RFC_Nom,RFC_Fecha),
+    FOREIGN KEY (Num_telefonico) REFERENCES Cliente(Num_telefonico),
+    FOREIGN KEY (No_guisado) REFERENCES Menu(No_guisado)
 );
 
 ---ALTER TABLE Pedido MODIFY Subtotal NUMBER(6,2);
--- PROYECTO-Cocina/Create-DB.sql
+++ PROYECTO-Cocina/Create-DB.sql
@@ -12,11 +12,52 @@ ALTER PLUGGABLE DATABASE Cocina SAVE STATE;
 -- Step 4: Connect to the Pluggable Database
 ALTER SESSION SET CONTAINER = Cocina;
 
--- Step 5: Create first admin user
-CREATE USER yaeladmin IDENTIFIED BY "102005";
+
+--===========================================
+CREATE USER yaeladmin IDENTIFIED BY "i12705fgf214";
 
 -- Grant necessary privileges to yaeladmin
 GRANT DBA TO yaeladmin;
 GRANT CONNECT, RESOURCE TO yaeladmin;
 GRANT CREATE SESSION TO yaeladmin;
 GRANT UNLIMITED TABLESPACE TO yaeladmin;
+
+--============================================
+
+CREATE USER itza IDENTIFIED BY "925lbkv1vl9m";
+
+-- Grant necessary privileges to itza
+GRANT DBA TO itza;
+GRANT CONNECT, RESOURCE TO itza;
+GRANT CREATE SESSION TO itza;
+GRANT UNLIMITED TABLESPACE TO itza;
+ 
+--===============================================
+
+CREATE USER sofi IDENTIFIED BY "tp451thu0c2c";
+
+-- Grant necessary privileges to sofi
+GRANT DBA TO sofi;
+GRANT CONNECT, RESOURCE TO sofi;
+GRANT CREATE SESSION TO sofi;
+GRANT UNLIMITED TABLESPACE TO sofi;
+
+--==============================================
+CREATE USER mary IDENTIFIED BY "93i1xta1fnwz";
+
+-- Grant necessary privileges to mary
+GRANT DBA TO mary;
+GRANT CONNECT, RESOURCE TO mary;
+GRANT CREATE SESSION TO mary;
+GRANT UNLIMITED TABLESPACE TO mary;
+
+--===============================================
+CREATE USER betsa18 IDENTIFIED BY "09rk2uozceqr";
+
+-- Grant necessary privileges to betsa18
+GRANT DBA TO betsa18;
+GRANT CONNECT, RESOURCE TO betsa18;
+GRANT CREATE SESSION TO betsa18;
+GRANT UNLIMITED TABLESPACE TO betsa18;
+
+--=============================================
